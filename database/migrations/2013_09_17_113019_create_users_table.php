d51b0c0c6843a9dc6230cf25e05cbfb57a3c7a23
DESKTOP-IEO0OPV\mansilla <sk8andrewxd@gmail.com>
2022-08-06 02:55:59 -0500

    bug primary key in migrations

--- database/migrations/2013_09_17_113019_create_users_table.php
+++ database/migrations/2013_09_17_113019_create_users_table.php
@@ -3,11 +3,12 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
-
+use Illuminate\Support\Facades\DB;
 return new class extends Migration
 {
     public function up()
     {
+        DB::statement('SET SESSION sql_require_primary_key=0'); 
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
             $table->string('github_id')->default('');
