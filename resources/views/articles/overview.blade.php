82b851223879e8fd55a7fc74502851ed1a712958
DESKTOP-2D20V62\Support <johnmansillaprada@gmail.com>
2022-08-06 12:16:49 -0500

    css

--- resources/views/articles/overview.blade.php
+++ resources/views/articles/overview.blade.php
@@ -10,7 +10,7 @@
         </div>
     </div>
 
-    <div class="bg-lio-100">
+    <div class="bg-[#204051]">
         <div class="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:py-20 lg:px-8 lg:flex lg:items-center lg:justify-between">
             <h2 class="text-2xl font-extrabold tracking-tight text-white md:text-3xl">
                 <span class="block">¿Algún conocimiento por compartir?</span>
